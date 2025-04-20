b70a4be510eab8d8d8a4bdaa12529f3056fba3b4
voc94 <popavasile02@yahoo.com>
2025-04-20 19:53:46 +0300

    Grayscaled Image Output

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/arduino esp s3/esp-who-master/examples/human_face_detect/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Espressif/frameworks/esp-idf-v5.4.1-2/.git")
+set(GIT_DIR "D:/arduino esp s3/esp-who-master/examples/human_face_detect/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
